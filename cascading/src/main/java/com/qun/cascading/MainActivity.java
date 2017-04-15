20a4de1e6c929635aa13b80242e7bb2bb43b4c87
Qun <slvt@qq.com>
2017-04-16 01:38:03 +0800

    初始化布局

--- cascading/src/main/java/com/qun/cascading/MainActivity.java
+++ cascading/src/main/java/com/qun/cascading/MainActivity.java
@@ -1,13 +1,25 @@
 package com.qun.cascading;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEtAddress;
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEtAddress = (EditText) findViewById(R.id.et_address);
+        mBtn = (Button) findViewById(R.id.btn);
+    }
+
+    public void selectAddress(View view) {
+
     }
 }
