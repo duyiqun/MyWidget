6441c9234308029c84a961b84d1cdeb3f598082a
Qun <slvt@qq.com>
2017-04-17 00:20:17 +0800

    完善代码

--- myviewpager/src/main/java/com/qun/myviewpager/MainActivity.java
+++ myviewpager/src/main/java/com/qun/myviewpager/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int[] resIds = {R.mipmap.a1,R.mipmap.a2,R.mipmap.a3,R.mipmap.a4,R.mipmap.a5,R.mipmap.a6};
+    private MyViewPager mMyViewPager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMyViewPager = (MyViewPager) findViewById(R.id.myViewPager);
+        mMyViewPager.setData(resIds);
     }
 }
--- myviewpager/src/main/java/com/qun/myviewpager/MyViewPager.java
+++ myviewpager/src/main/java/com/qun/myviewpager/MyViewPager.java
@@ -1,5 +1,7 @@
 package com.qun.myviewpager;
 
+import android.content.Context;
+import android.util.AttributeSet;
 import android.view.ViewGroup;
 
 /**
@@ -7,4 +9,35 @@ import android.view.ViewGroup;
  */
 
 public class MyViewPager extends ViewGroup {
+
+    public MyViewPager(Context context) {
+        this(context, null);
+    }
+
+    public MyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+
+    }
+
+    public MyViewPager(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs);
+    }
+
+    public MyViewPager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        this(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+    /**
+     * 将图片放到ImageView上，然后经ImageView添加到当前ViewGroup中
+     * @param resIds
+     */
+    public void setData(int[] resIds) {
+        
+    }
 }
