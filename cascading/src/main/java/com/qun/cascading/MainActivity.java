9733d6e1a72add153c63e91124919cfde3486b34
Qun <slvt@qq.com>
2017-04-16 20:00:17 +0800

    初始化PopupWindow

--- cascading/src/main/java/com/qun/cascading/MainActivity.java
+++ cascading/src/main/java/com/qun/cascading/MainActivity.java
@@ -1,21 +1,32 @@
 package com.qun.cascading;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupWindow;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String DB_NAME = "city.s3db";
     private EditText mEtAddress;
     private Button mBtn;
+    private List<Address> provinceList = new ArrayList<>();
+    private List<Address> cityList = new ArrayList<>();
+    private List<Address> districtList = new ArrayList<>();
+    private PopupWindow mPopupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         mEtAddress = (EditText) findViewById(R.id.et_address);
         mBtn = (Button) findViewById(R.id.btn);
         initDB();
+        initProvince();
+        initPopupView();
     }
 
     /**
@@ -53,8 +66,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void selectAddress(View view) {
+    private void initProvince() {
+        provinceList.clear();
+        SQLiteDatabase database = openOrCreateDatabase(DB_NAME, MODE_PRIVATE, null);
+        Cursor cursor = database.rawQuery("select code,name from province", null);
+        while (cursor.moveToNext()) {
+            Address address = new Address();
+            address.code = cursor.getString(0);
+            address.name = cursor.getString(1);
+            provinceList.add(address);
+        }
+        cursor.close();
+        database.close();
+    }
 
+    private void initPopupView() {
+        View popupView = LayoutInflater.from(this).inflate(R.layout.popup_layout, null);
+        mPopupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, true);
+    }
+
+    public void selectAddress(View view) {
+        //popupView依附的锚点View
+        mPopupWindow.showAsDropDown(mBtn, 0, 0);
 
     }
 }
