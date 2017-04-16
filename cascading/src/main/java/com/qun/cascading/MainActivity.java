94cf59d590bcccceb2fb490e714ec263e66e8ac7
Qun <slvt@qq.com>
2017-04-16 13:59:36 +0800

    复制assets中数据库到/data/data

--- cascading/src/main/java/com/qun/cascading/MainActivity.java
+++ cascading/src/main/java/com/qun/cascading/MainActivity.java
@@ -6,8 +6,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String DB_NAME = "city.s3db";
     private EditText mEtAddress;
     private Button mBtn;
 
@@ -17,9 +23,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mEtAddress = (EditText) findViewById(R.id.et_address);
         mBtn = (Button) findViewById(R.id.btn);
+        initDB();
+    }
+
+    /**
+     * 1. 需要将assets目录下的数据库文件拷贝到/data/data/包名/databases/目录下才能被以很简单的API访问
+     * SQLiteDatabase.openDatabase()
+     */
+    private void initDB() {
+        //data/data/包名/databases/
+        try {
+            //注意：系统默认没有databases目录，因此需要先创建这个目录才能在这个目录下创建文件
+            File dbDir = new File("/data/data/" + getPackageName() + "/databases");
+            if (!dbDir.exists()) {
+                dbDir.mkdirs();
+            }
+            InputStream is = getAssets().open(DB_NAME);
+            File dbFile = new File(dbDir, DB_NAME);
+            FileOutputStream fos = new FileOutputStream(dbFile);
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            while ((len = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, len);
+            }
+            fos.close();
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void selectAddress(View view) {
+
 
     }
 }
