c3d7171b9dacd9d699a60ba3be7bd05f3878240a
Qun <slvt@qq.com>
2017-04-17 22:33:16 +0800

    添加onTouchEventListener

--- eventdispatch/src/main/java/com/qun/eventdispatch/MainActivity.java
+++ eventdispatch/src/main/java/com/qun/eventdispatch/MainActivity.java
@@ -1,14 +1,27 @@
 package com.qun.eventdispatch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ViewGroupB mViewGroupB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mViewGroupB = (ViewGroupB) findViewById(R.id.viewGroupB);
+        mViewGroupB.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d(TAG, "onTouch: " + event.getAction());
+                return false;
+            }
+        });
     }
 }
