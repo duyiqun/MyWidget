e74902f01214175f0989b40fba24038bdc9524e0
Qun <slvt@qq.com>
2017-04-17 21:52:06 +0800

    事件分发流程

---
+++ eventdispatch/src/main/java/com/qun/eventdispatch/MyView.java
@@ -0,0 +1,38 @@
+package com.qun.eventdispatch;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Qun on 2017/4/17.
+ */
+
+public class MyView extends View {
+    private static final String TAG = "MyView";
+
+    public MyView(Context context) {
+        this(context, null);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        this(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent: " + event.getAction());
+        return super.onTouchEvent(event);
+    }
+}
---
+++ eventdispatch/src/main/java/com/qun/eventdispatch/ViewGroupA.java
@@ -0,0 +1,44 @@
+package com.qun.eventdispatch;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Qun on 2017/4/17.
+ */
+
+public class ViewGroupA extends RelativeLayout {
+    private static final String TAG = "ViewGroupA";
+
+    public ViewGroupA(Context context) {
+        this(context, null);
+    }
+
+    public ViewGroupA(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public ViewGroupA(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs);
+    }
+
+    public ViewGroupA(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        this(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        Log.d(TAG, "onInterceptTouchEvent: " + ev.getAction());
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent: " + event.getAction());
+        return super.onTouchEvent(event);
+    }
+}
