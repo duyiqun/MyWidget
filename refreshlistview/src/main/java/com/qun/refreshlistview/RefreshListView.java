d63b896c58d4b093a2a25fae10d35491db8b6b27
Qun <slvt@qq.com>
2017-04-18 09:49:13 +0800

    创建refreshlistview类

---
+++ refreshlistview/src/main/java/com/qun/refreshlistview/RefreshListView.java
@@ -0,0 +1,28 @@
+package com.qun.refreshlistview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by Qun on 2017/4/18.
+ */
+
+public class RefreshListView extends ListView {
+    public RefreshListView(Context context) {
+        this(context, null);
+    }
+
+    public RefreshListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        
+    }
+
+    public RefreshListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs);
+    }
+
+    public RefreshListView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        this(context, attrs, defStyleAttr);
+    }
+}
