c492d758b0766c7fbf4a20d8b6450c6dc3aba7fa
Qun <slvt@qq.com>
2017-04-18 11:20:12 +0800

    添加HeaderView

--- refreshlistview/src/main/java/com/qun/refreshlistview/RefreshListView.java
+++ refreshlistview/src/main/java/com/qun/refreshlistview/RefreshListView.java
@@ -2,6 +2,8 @@ package com.qun.refreshlistview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ListView;
 
 /**
@@ -9,13 +11,16 @@ import android.widget.ListView;
  */
 
 public class RefreshListView extends ListView {
+
+    private View mHeaderView;
+
     public RefreshListView(Context context) {
         this(context, null);
     }
 
     public RefreshListView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
+        initHeaderView();
     }
 
     public RefreshListView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -24,5 +29,12 @@ public class RefreshListView extends ListView {
 
     public RefreshListView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         this(context, attrs, defStyleAttr);
+    }
+
+    private void initHeaderView() {
+        mHeaderView = LayoutInflater.from(getContext()).inflate(R.layout.header_layout, this, false);
+        //将headerView添加到ListView的头部（这是ListView本身支持的功能而RecyclerView没有这样的功能）
+        addHeaderView(mHeaderView);
+
     }
 }
