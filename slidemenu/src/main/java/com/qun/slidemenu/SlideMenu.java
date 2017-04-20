40d1bfc948dbf3f8bb59df28ec5ecaa9c65c41f3
Qun <slvt@qq.com>
2017-04-21 01:31:27 +0800

    完成SlideMenu

--- slidemenu/src/main/java/com/qun/slidemenu/SlideMenu.java
+++ slidemenu/src/main/java/com/qun/slidemenu/SlideMenu.java
@@ -2,9 +2,12 @@ package com.qun.slidemenu;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Scroller;
 
 /**
  * Created by Qun on 2017/4/19.
@@ -12,6 +15,7 @@ import android.view.ViewGroup;
 
 public class SlideMenu extends ViewGroup {
 
+    private static final String TAG = "SlideMenu";
     private View mMenuView;
     private View mMainView;
     private int mMenuWidth;
@@ -20,6 +24,10 @@ public class SlideMenu extends ViewGroup {
     private int mMainHeight;
     private float mStartX;
     private float mCurrentX;
+    private Scroller mScroller;
+    private float mStartX2;
+    private float mStartY2;
+    private GestureDetector mGestureDetector;
 
     public SlideMenu(Context context) {
         this(context, null);
@@ -27,8 +35,68 @@ public class SlideMenu extends ViewGroup {
 
     public SlideMenu(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mScroller = new Scroller(context);
+        /**
+         * 1. 创建手势识别器对象
+         */
+        mGestureDetector = new GestureDetector(context, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+//                Log.d(TAG, "onDown: " + e.getAction());
+                return true;
+            }
 
+            @Override
+            public void onShowPress(MotionEvent e) {
+//                Log.d(TAG, "onShowPress: " + e.getAction());
+            }
 
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+//                Log.d(TAG, "onSingleTapUp: " + e.getAction());
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                Log.d(TAG, "onScroll: " + distanceX);
+                float dx = mStartX - e2.getX();
+                mStartX = e2.getX();
+
+                if (getScrollX() + dx < -mMenuWidth) {
+                    scrollTo(-mMenuWidth, 0);
+                } else if (getScrollX() + dx > 0) {
+                    scrollTo(0, 0);
+                } else {
+                    scrollBy((int) dx, 0);
+                }
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+//                Log.d(TAG, "onLongPress: " + e.getAction());
+            }
+
+            /**
+             * 相当于Up事件
+             * @param e1
+             * @param e2
+             * @param velocityX
+             * @param velocityY
+             * @return
+             */
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+//                Log.d(TAG, "onFling: velocityX="+velocityX);
+//                if (getScrollX() < -mMenuWidth / 2) {
+//                    open();
+//                } else {
+//                    close();
+//                }
+                return false;
+            }
+        });
     }
 
     public SlideMenu(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -59,24 +127,98 @@ public class SlideMenu extends ViewGroup {
         mMainView.layout(0, 0, mMainWidth, mMainHeight);
     }
 
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        switch (event.getAction()) {
+//            case MotionEvent.ACTION_DOWN:
+//                mStartX = event.getX();
+//                break;
+//            case MotionEvent.ACTION_MOVE:
+//                mCurrentX = event.getX();
+//                float dx = mStartX - mCurrentX;
+//                mStartX = mCurrentX;
+//                float newScrollX = getScrollX() + dx;
+//                if (newScrollX < -mMenuWidth) {
+//                    scrollTo(-mMenuWidth, 0);
+//                } else if (newScrollX > 0) {
+//                    scrollTo(0, 0);
+//                } else {
+//                    scrollBy((int) dx, 0);
+//                }
+//                break;
+//            case MotionEvent.ACTION_UP:
+//                if (getScrollX() < -mMenuWidth / 2) {
+//                    open();
+//                } else {
+//                    close();
+//                }
+//                break;
+//            default:
+//                break;
+//        }
+//        return true;
+//    }
+
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (getScrollX() < -mMenuWidth / 2) {
+                open();
+            } else {
+                close();
+            }
+        }
+        return mGestureDetector.onTouchEvent(event);
+    }
+
+    private void open() {
+        int dx = -mMenuWidth - getScrollX();
+        mScroller.startScroll(getScrollX(), 0, dx, 0, Math.abs(dx));
+        invalidate();
+    }
+
+    private void close() {
+        int dx = -getScrollX();
+        mScroller.startScroll(getScrollX(), 0, dx, 0, Math.abs(dx));
+        invalidate();
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (mScroller.computeScrollOffset()) {
+            int currX = mScroller.getCurrX();
+            scrollTo(currX, 0);
+            invalidate();
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                mStartX = event.getX();
+                mStartX2 = ev.getX();
+                mStartY2 = ev.getY();
+                //保存down事件时的x的位置
+                mStartX = ev.getX();
                 break;
             case MotionEvent.ACTION_MOVE:
-                mCurrentX = event.getX();
-                float dx = mStartX - mCurrentX;
-                mStartX = mCurrentX;
-                scrollBy((int) dx, 0);
+                float currentX = ev.getX();
+                float currentY = ev.getY();
+                float dx = currentX - mStartX2;
+                float dy = currentY - mStartY2;
+                mStartX2 = currentX;
+                mStartY2 = currentY;
+                if (Math.abs(dx) > Math.abs(dy)) {//水平
+                    return true;
+                }
                 break;
             case MotionEvent.ACTION_UP:
-
                 break;
             default:
                 break;
         }
-        return true;
+        return false;
     }
 }
